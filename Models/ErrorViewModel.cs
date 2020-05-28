40e691d786a9650fa97e41e0050da9a0c8057a42
feher.attila96@gmail.com <feher.attila96@gmail.com>
2020-05-28 13:21:36 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace tic_tac_toe.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
